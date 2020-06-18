d717c3d35f599003f009f80333f2fd26840e78be
zanderz <alexander.zaitzeff@twosixlabs.com>
2020-06-18 15:15:35 -0400

    Made suggested changes from two merge requests ago

--- tests/test_local_handler.py
+++ tests/test_local_handler.py
@@ -1,16 +1,19 @@
 import pytest
 
-from datastorer.local_handler import LocalHandler
+from datastorer.local_handler import LocalCSVHandler
 
 
 @pytest.fixture()
 def make_local_handler():
-    got_data = LocalHandler("tests/test_data/penguins_size_small")
+    got_data = LocalCSVHandler("tests/test_data/penguins_size_small")
     return got_data
 
 
 def test_local_handler_init(make_local_handler):
-    assert make_local_handler.file_path == "tests/test_data/penguins_size_small/penguins_size_small.csv"
+    assert (
+        make_local_handler.file_path
+        == "tests/test_data/penguins_size_small/penguins_size_small.csv"
+    )
 
 
 def test_get_column_names(make_local_handler):
@@ -31,5 +34,3 @@ def test_get_column_data(make_local_handler):
     test_dict = make_local_handler.get_column_data(data_dict)
     assert (test_dict["x"] == [3750, 3800, 3250]).all()
     assert (test_dict["y"] == [181, 186, 195]).all()
-
-
