b10c2d2b18e40f36116a2c6c5dbf5e9ff55c567c
Nick Leiby <nick.leiby@twosixlabs.com>
2020-06-18 14:24:57 -0400

    Adds temporary data backend selector for sql vs local filesystem config- don't force a db connection if it's not set up.

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ from sqlalchemy.engine.url import URL
 from app_settings import DATABASE_CONFIG
 
 
-def create_app():
+def create_app(backend):
     app = Flask(__name__)
     # todo: option to build app without the sql connection if it's not needed. We'll build the config mapping based on info in the user-generated app config
     app.config.from_mapping(
         SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or URL(**DATABASE_CONFIG),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         VERSION="0.0.1")
-
-    db = SQLAlchemy()
-    db.init_app(app)
+    if backend == 'sql':
+        db = SQLAlchemy()
+        db.init_app(app)
 
     # register url bleuprints with the app object
     from view import bp as sub_bp
@@ -26,12 +26,11 @@ def create_app():
     return app
 
 
-app = create_app()
-
 if __name__ == "__main__":
+    backend = 'filesystem'
     if len(sys.argv) > 1:
-        app.config["path_to_json"] = sys.argv[1]  # sanitize inputs
-    else:
-        app.config["path_to_json"] = "tests/test_data/test_app_config.json"
+        backend = sys.argv[1]
 
+    app = create_app(backend)
+    app.config["path_to_json"] = "tests/test_data/test_app_config.json"
     app.run()
