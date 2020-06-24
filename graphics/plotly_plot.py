2cb4dfede37eae3e79e4a179ba41ab746c3f5820
zanderz <alexander.zaitzeff@twosixlabs.com>
2020-06-24 10:49:43 -0400

    Simplified the plotly code

--- graphics/plotly_plot.py
+++ graphics/plotly_plot.py
@@ -1,18 +1,39 @@
 import json
-
+import plotly
+from flask import render_template
 from graphics.graphic_class import Graphic
 
-import plotly
+HOVER_TEMPLATE_HTML = "hover_template.html"
 
 DATA = "data"
 LAYOUT = "layout"
 AXIS = "{}axis"
 TITLE = "title"
+CUSTOM_DATA = "customdata"
+HOVER_TEMPLATE = "hovertemplate"
+
+
+def get_hover_data_in_plotly_form(data, hover_column_names):
+    """
+    if data is a dataframe:
+    plot_options[DATA][index]["customdata"] = data[hover_data].values.tolist()
+    is equalavent to this function
+
+    :param data:
+    :param hover_column_names:
+    :return:
+    """
+
+    hover_data_list = []
+    for hover_col_name in hover_column_names:
+        hover_data_list.append(data[hover_col_name])
+
+    return list(map(list, zip(*hover_data_list)))  # transpose list of lists
 
 
 class PlotlyPlot(Graphic):
     def make_dict_for_html_plot(
-        self, data, axis_to_data_columns, plot_options, hover_data=None
+        self, data, axis_to_data_columns, plot_options, hover_column_names=None
     ):
 
         for index, axis_to_data_dict in enumerate(axis_to_data_columns):
@@ -26,23 +47,14 @@ class PlotlyPlot(Graphic):
                     else:
                         plot_options[LAYOUT] = {AXIS.format(axis): {TITLE: column_name}}
 
-            if hover_data is not None:
-                hover_data_list = []
-                hover_template_list = []
-                for hover_index, hover_col in enumerate(hover_data):
-                    hover_data_list.append(data[hover_col])
-                    hover_template_list.append(
-                        "".join([hover_col, ": %{customdata[", str(hover_index), "]}"])
-                    )
-                hover_template_list.append("<extra></extra>")
-                plot_options[DATA][index]["hovertemplate"] = "<br>".join(
-                    hover_template_list
-                )
-                plot_options[DATA][index]["customdata"] = list(
-                    map(list, zip(*hover_data_list))
-                )  # transpose
+            if hover_column_names is None:
+                continue
+            plot_options[DATA][index][CUSTOM_DATA] = get_hover_data_in_plotly_form(
+                data, hover_column_names
+            )
+            plot_options[DATA][index][HOVER_TEMPLATE] = render_template(
+                HOVER_TEMPLATE_HTML, hover_column_names=hover_column_names
+            )
 
-        graph_json = json.dumps(
-            plot_options, cls=plotly.utils.PlotlyJSONEncoder
-        )  # I decided that I want to keep the strings here
+        graph_json = json.dumps(plot_options, cls=plotly.utils.PlotlyJSONEncoder)
         return graph_json
