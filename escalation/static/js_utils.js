def68d52dbc38985ad29c1e639a689a10017c3c5
zanderz <alexander.zaitzeff@twosixlabs.com>
2020-07-20 13:12:18 -0400

    added licensed

--- escalation/static/js_utils.js
+++ escalation/static/js_utils.js
@@ -1,3 +1,6 @@
+// Copyright [2020] [Two Six Labs, LLC]
+// Licensed under the Apache License, Version 2.0
+
 function post_to_server(id_on_web_page) {
     //This allows the web page to focus where the plot was updated instead of starting at the top of the web page
     let web_form=$('#form_'.concat(id_on_web_page));
