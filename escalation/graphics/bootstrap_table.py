7cad84f934ccd488e79da2437dea6b13937ad033
zanderz <alexander.zaitzeff@twosixlabs.com>
2020-08-17 18:35:48 -0400

    You can now use tables

---
+++ escalation/graphics/bootstrap_table.py
@@ -0,0 +1,61 @@
+# Copyright [2020] [Two Six Labs, LLC]
+# Licensed under the Apache License, Version 2.0
+
+import json
+import plotly
+from flask import render_template
+from graphics.graphic_class import Graphic
+from utility.constants import COLUMN_NAME, DATA, LAYOUT, OPTIONS
+
+HEADER_INFO = "header_info"
+
+
+class BootstrapTable(Graphic):
+    def make_dict_for_html_plot(self, data, plot_options, visualization_options=None):
+        """
+        Makes the dictionary that bootstrap_table.html takes in
+        options is currenlty not implemented
+        based on https://bootstrap-table.com/
+        :param data: a pandas dataframe
+        :param axis_to_data_columns:
+        :param plot_options:
+        :param visualization_options: not used
+        :return:
+        """
+        list_of_column_names = self.remove_duplicate_column_names(plot_options[DATA])
+        table_dict = {
+            HEADER_INFO: plot_options[DATA],
+            DATA: json.dumps(data[list_of_column_names].to_dict("records")),
+            OPTIONS: plot_options.get(OPTIONS, {}),
+        }
+        return table_dict
+
+    def get_data_columns(self, plot_options) -> set:
+        """
+        extracts what columns of data are needed from the plot_options
+        :param plot_options:
+        :return:
+        """
+        set_of_column_names = set()
+        for dict_of_data_for_each_column in plot_options[DATA]:
+            set_of_column_names.add(dict_of_data_for_each_column[COLUMN_NAME])
+        return set_of_column_names
+
+    def remove_duplicate_column_names(self, list_of_dict_of_header_info):
+        """
+        No two columns of the table can have the same data
+        :param plot_options:
+        :return:
+        """
+        list_of_column_names = []
+        i = 0
+        n = len(list_of_dict_of_header_info)
+        while i < n:
+            dict_of_header_info = list_of_dict_of_header_info[i]
+            if dict_of_header_info[COLUMN_NAME] in list_of_column_names:
+                del list_of_dict_of_header_info[i]
+                n = n - 1
+            else:
+                list_of_column_names.append(dict_of_header_info[COLUMN_NAME])
+                i = i + 1
+        return list_of_column_names
--- escalation/graphics/utils/available_graphics.py
+++ escalation/graphics/utils/available_graphics.py
@@ -2,65 +2,16 @@
 # Licensed under the Apache License, Version 2.0
 
 from graphics.plotly_plot import PlotlyPlot
+from graphics.bootstrap_table import BootstrapTable
 
 """
 List of the available graphics
 """
 
 AVAILABLE_GRAPHICS = {
-    "plotly": {"object": PlotlyPlot(), "graph_html_template": "plotly.html"}
-}
-
-"""
-I am going to repurpose this when I am making admin UI set up
-AVAILABLE_GRAPHICS = {
-    "table": {
-        "object": PlotlyPlot(),
-        "num_data": -1,
-        "show_name": "Table",
-        "Reference": "https://plotly.com/javascript/reference/#table",
-    },
-    "bar": {
-        "object": PlotlyPlot("bar"),
-        "num_data": 2,
-        "show_name": "Bar Plot",
-        "Reference": "https://plotly.com/javascript/reference/#scatter",
-    },
-    "heatmap": {
-        "object": PlotlyPlot("heatmap"),
-        "num_data": 3,
-        "show_name": "Heat map",
-        "Reference": "https://plotly.com/javascript/reference/#heatmap",
-    },
-    "scatter": {
-        "object": PlotlyPlot("scatter"),
-        "num_data": 2,
-        "show_name": "Scatter/Line Plot",
-        "Reference": "https://plotly.com/javascript/reference/#heatmap",
-    },
-    "box": {
-        "object": PlotlyPlot("box"),
-        "num_data": 2,
-        "show_name": "Box",
-        "Reference": "https://plotly.com/javascript/reference/#box",
-    },
-    "histogram": {
-        "object": PlotlyPlot("histogram"),
-        "num_data": 1,
-        "show_name": "Histogram",
-        "Reference": "https://plotly.com/javascript/reference/#histogram",
-    },
-    "scatter3d": {
-        "object": PlotlyPlot("scatter3d"),
-        "num_data": 3,
-        "show_name": "3D Scatter Plot",
-        "Reference": "https://plotly.com/javascript/reference/#scatter3d",
-    },
-    "surface": {
-        "object": PlotlyPlot("surface"),
-        "num_data": 3,
-        "show_name": "Surface Plot",
-        "Reference": "https://plotly.com/javascript/reference/#surface",
+    "plotly": {"object": PlotlyPlot(), "graph_html_template": "plotly.html"},
+    "bootstrap-table": {
+        "object": BootstrapTable(),
+        "graph_html_template": "bootstrap-table.html",
     },
 }
-"""
