f87093fecc696ecfb44366aa5526fd67ef00e510
Nick Leiby <nick.leiby@twosixlabs.com>
2020-09-03 15:58:48 -0400

    Update local csv handler upload with metadata write

--- escalation/database/local_handler.py
+++ escalation/database/local_handler.py
@@ -33,6 +33,7 @@ from utility.constants import (
     TABLE_NAME,
     USERNAME,
     NOTES,
+    DATETIME_FORMAT,
 )
 
 
@@ -96,10 +97,14 @@ class LocalCSVHandler(DataHandler):
         return filepaths_list
 
     @staticmethod
-    def build_df_for_data_source_type(data_source):
+    def load_df_from_csv(filepath):
+        return pd.read_csv(filepath, comment="#")
+
+    @classmethod
+    def build_df_for_data_source_type(cls, data_source):
         # There may be multiple files for a particular data source type
         data_source_df = pd.concat(
-            [pd.read_csv(filepath) for filepath in data_source[DATA_LOCATION]]
+            [cls.load_df_from_csv(filepath) for filepath in data_source[DATA_LOCATION]]
         )
         # add the data_source/table name as a prefix to disambiguate columns
         data_source_df = data_source_df.add_prefix(
@@ -211,7 +216,9 @@ class LocalCSVDataInventory(LocalCSVHandler):
             )
             list_of_files = glob.glob(f"{full_path}/*.csv")
             assert len(list_of_files) > 0
-            files_by_data_source[data_source_name] = list_of_files
+            files_by_data_source[data_source_name] = [
+                os.path.basename(f) for f in list_of_files
+            ]
         identifiers_by_table = defaultdict(list)
         # for each file identifier in the file system, match up any metadata we have
         for data_source_name, data_source_identifiers in files_by_data_source.items():
@@ -293,30 +300,38 @@ class LocalCSVDataInventory(LocalCSVHandler):
         latest_filepath = max(list_of_files, key=os.path.getctime)
         return pd.read_csv(latest_filepath, nrows=1).columns.tolist()
 
-    def write_data_upload_to_backend(self, uploaded_data_df, file_name=None):
+    def write_data_upload_to_backend(self, uploaded_data_df, username, notes):
         """
-        :param file_name:
         :param uploaded_data_df: pandas dataframe on which we have already done validation
         :param data_source_name:
 
         :return: Empty list representing columns not in the new file that are in the old. Included for function signature matching
         """
+        # todo: add filename arg back in, and check if filename exists and raise exception if it does- don't overwrite existing files
+        upload_time = datetime.utcnow()
+        file_name = upload_time.strftime(DATETIME_FORMAT) + ".csv"
 
-        file_name = (
-            datetime.utcnow().strftime("%Y%m%d-%H%M%S")
-            if (file_name is None)
-            else sanitize_filename(secure_filename(file_name))
+        directory = os.path.join(
+            current_app.config[CONFIG_FILE_FOLDER], DATA, self.data_source_name
         )
-        file_name = (
-            file_name if file_name.endswith(".csv") else "".join([file_name, ".csv"])
-        )
-
-        file_path = os.path.join(
-            current_app.config[CONFIG_FILE_FOLDER],
-            DATA,
-            self.data_source_name,
-            file_name,
-        )
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        file_path = os.path.join(directory, file_name)
         uploaded_data_df.to_csv(file_path)
-        # todo: write upload to metadata file
+
+        # update the data upload metadata
+        data_upload_metadata = self.get_data_upload_metadata_df()
+        new_row = {
+            UPLOAD_ID: file_name,
+            TABLE_NAME: self.data_source_name,
+            USERNAME: username,
+            NOTES: notes,
+            ACTIVE: True,
+            UPLOAD_TIME: upload_time,
+        }
+        data_upload_metadata = pd.concat(
+            [data_upload_metadata, pd.DataFrame([new_row])]
+        )
+        data_upload_metadata.to_csv(self.get_data_upload_metadata_path(), index=False)
+
         return []
